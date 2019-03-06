c7098391dbeced70e0b6ff06c9e6fd43f9810208
Per Låås <per.laas@sprakochfolkminnen.se>
2019-03-06 10:08:28 +0100

    https everywhere. Check: find . -type f | xargs grep 'frigg' or xargs grep 'sprakoch'.

--- scripts/config.js
+++ scripts/config.js
@@ -30,20 +30,20 @@ export default {
 		feedbackText: 'Har du hittat några fel i kartan? Har du kompletterande information om frågelistsvaren eller personerna som nämns? Kontakta oss gärna!',
 		feedbackEmail: 'matkult',
 
-		copyrightContent: '<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-nd/2.5/se/"><img alt="Creative Commons-licens" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/2.5/se/88x31.png" /></a><br />Detta verk är licensierat under en <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-nd/2.5/se/">Creative Commons Erkännande-IngaBearbetningar 2.5 Sverige Licens</a>.'
+		copyrightContent: '<a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-nd/2.5/se/"><img alt="Creative Commons-licens" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/2.5/se/88x31.png" /></a><br />Detta verk är licensierat under en <a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-nd/2.5/se/">Creative Commons Erkännande-IngaBearbetningar 2.5 Sverige Licens</a>.'
 	},
 
 	// Webbsida som kommer visas i en popup-ruta när folk kommer först till kartan
-	startPageUrl: 'http://www.sprakochfolkminnen.se/om-oss/kartor/matkartan/om-matkartan---for-popup-ruta-pa-sjalva-kartan.html',
+	startPageUrl: 'https://www.sprakochfolkminnen.se/om-oss/kartor/matkartan/om-matkartan---for-popup-ruta-pa-sjalva-kartan.html',
 
 	country: 'sweden',
 
-	imageUrl: 'http://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/',
-	audioUrl: 'http://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/inspelningar/',
+	imageUrl: 'https://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/',
+	audioUrl: 'https://www4.sprakochfolkminnen.se/Folkminnen/Svenska_sagor_filer/dialekter/',
 
-	appUrl: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/',
-	siteUrl: 'http://www.sprakochfolkminnen.se/matkult/kartan.html',
+	appUrl: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/',
+	siteUrl: 'https://www.sprakochfolkminnen.se/matkult/kartan.html',
 
-	apiUrl: 'http://frigg.sprakochfolkminnen.se/sagendatabas/api/es/',
-	restApiUrl: 'http://frigg.sprakochfolkminnen.se/sagendatabas/api/'
+	apiUrl: 'https://frigg.sprakochfolkminnen.se/sagendatabas/api/es/',
+	restApiUrl: 'https://frigg.sprakochfolkminnen.se/sagendatabas/api/'
 };
--- scripts/utils/matkartaCategories.js
+++ scripts/utils/matkartaCategories.js
@@ -18,62 +18,62 @@ export default {
 		{
 			category: 'MK-6',
 			label: 'Baljväxter',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/baljvaxter.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/baljvaxter.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/baljvaxter.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/baljvaxter.html'
 		},
 		{
 			category: 'MK-3',
 			label: 'Bröd',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/brod.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/brod.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/brod.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/brod.html'
 		},
 		{
 			category: 'MK-2',
 			label: 'Bär och frukt',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/barochfrukt.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/bar-och-frukt.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/barochfrukt.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/bar-och-frukt.html'
 		},
 		{
 			category: 'MK-5',
 			label: 'Honung',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/honung.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/honung.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/honung.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/honung.html'
 		},
 		{
 			category: 'MK-7',
 			label: 'Kål',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/kal.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/kal.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/kal.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/kal.html'
 		},
 		{
 			category: 'MK-1',
 			label: 'Mjölk',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/mjolk.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/mjolk.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/mjolk.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/mjolk.html'
 		},
 		{
 			category: 'MK-8',
 			label: 'Potatis',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/potatis.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/potatis.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/potatis.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/potatis.html'
 		},
 		{
 			category: 'MK-4',
 			label: 'Öl och svagdricka',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/olochsvagdricka.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/ol-och-svagdricka.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/olochsvagdricka.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/ol-och-svagdricka.html'
 		},
 		{
 			category: 'MK-9',
 			label: 'Grödor och spannmål',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/grodorochspannmal.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/jordbruket-och-maten.html'
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/grodorochspannmal.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/jordbruket-och-maten.html'
 		},
 		{
 			category: 'MK-10',
 			label: 'Kokboken',
-			image: 'http://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/kokbok.jpg',
-			url: 'http://www.sprakochfolkminnen.se/matkult/kokboken.html',
+			image: 'https://frigg.sprakochfolkminnen.se/static/js-apps/matkartan/img/categories/kokbok.jpg',
+			url: 'https://www.sprakochfolkminnen.se/matkult/kokboken.html',
 			subCategories: [
 				{
 					category: 'MK-6',
