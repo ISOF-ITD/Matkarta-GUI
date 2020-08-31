ec2f5f5a929b592b07419634c21f8282536fe327
Rico Simke <rico.simke@isof.se>
2020-08-31 14:20:26 +0200

    swallowError in dev mode

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,14 @@ gulp.task('build', gulp.series('scripts', 'less'));
  
 // Private Functions
 // ----------------------------------------------------------------------------
+
+// source: https://stackoverflow.com/a/23973536
+function swallowError(error) {
+	// If you want details of the error in the console
+	console.log(error.toString())
+	this.emit('end')
+  }
+
 function bundleApp(isProduction) {
 	// Browserify will bundle all our js files together in to one and will let
 	// us use modules in the front end.
@@ -65,7 +73,7 @@ function bundleApp(isProduction) {
   		// transform ES6 and JSX to ES5 with babelify
 	  	.transform("babelify", {presets: ["@babel/preset-env", "@babel/preset-react"]})
 	    .bundle()
-	    .on('error', log)
+	    .on('error', isProduction ? log : swallowError)
 	    .pipe(source('app.js'))
     	.pipe(buffer())
         .pipe(gulpif(production, uglify()))
